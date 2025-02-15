da170b2c8539c2bc3805482c11247b2956177e8e
piyusdev2006 <piyushsingh955583@gmail.com>
2025-02-15 13:04:07 +0530

    Exposed url fixed

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,9 @@
 const mongoose = require("mongoose");
 
 // to connect to  cluster NamasteNoodejs ,simplr query is :
-
+const MONGODB_URI = process.env.MONGO_URI || "mongodb://localhost:27017/test";
 const connectToDB = async () => {
-   await mongoose.connect(
-      "mongodb+srv://NamasteNode:namaste123@akshaysaininode.ycri0.mongodb.net/devTinder"
-    ); 
+   await mongoose.connect(MONGODB_URI); 
 } 
 
 module.exports = { connectToDB };
