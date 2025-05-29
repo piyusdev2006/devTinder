d7a43e3fffe48a4720fb2ed80de03d1856073032
piyusdev2006 <piyushsingh955583@gmail.com>
2025-05-29 11:48:08 +0530

    feat: Use environment variable for MongoDB connection URI
    This commit updates the database configuration to use the `MONGODB_URI` environment variable for the MongoDB connection string.
    
    - Replaced the hardcoded MongoDB connection string with the `MONGODB_URI` environment variable in `src/config/database.js`. This allows for more flexible and secure configuration of the database connection.

--- src/config/database.js
+++ src/config/database.js
@@ -5,12 +5,11 @@ const mongoose = require("mongoose");
 
 // to connect to  cluster NamasteNoodejs ,simplr query is :
 
-
 const MONGODB_URI = process.env.MONGODB_URL;  // password updated with this "?retryWrites=true&w=majority"
 
 const connectToDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://NamasteNode:0ty4z3ZHLivba7qH@akshaysaininode.ycri0.mongodb.net/devTinder?retryWrites=true&w=majority"
+    MONGODB_URI
   );
 };
 
