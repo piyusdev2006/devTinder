4aaacad83737567a3f9add5253461e309a04149d
piyusdev2006 <piyushsingh955583@gmail.com>
2025-05-29 11:39:11 +0530

    fix: Hardcode MongoDB connection string in database configuration
    This commit temporarily hardcodes the MongoDB connection string in `src/config/database.js`.
    
    - Replaced the environment variable lookup for `MONGODB_URI` with a hardcoded connection string to ensure the application can connect to the database. This is a temporary fix and should be reverted to using environment variables in the future.

--- src/config/database.js
+++ src/config/database.js
@@ -4,11 +4,13 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 // to connect to  cluster NamasteNoodejs ,simplr query is :
+
+
 const MONGODB_URI = process.env.MONGODB_URL;  // password updated with this "?retryWrites=true&w=majority"
 
 const connectToDB = async () => {
   await mongoose.connect(
-    MONGODB_URI              
+    "mongodb+srv://NamasteNode:0ty4z3ZHLivba7qH@akshaysaininode.ycri0.mongodb.net/devTinder?retryWrites=true&w=majority"
   );
 };
 
