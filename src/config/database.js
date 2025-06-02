55f05283d694e6a03b9c95bbfade86a6fa3baa7d
piyusdev2006 <piyushsingh955583@gmail.com>
2025-06-02 17:45:05 +0530

    feat: Use MONGODB_URI for database connection
    This commit updates the database connection configuration to use the `MONGODB_URI` environment variable. The previous hardcoded connection string is removed. This aligns with the previous commit and ensures the application uses the environment variable for database connection.

--- src/config/database.js
+++ src/config/database.js
@@ -11,7 +11,7 @@ const MONGODB_URI = process.env.MONGODB_URL;
 
 const connectToDB = async () => {
   await mongoose.connect(
-    // "mongodb+srv://NamasteNode:ShOhQNzmIQq3z5JG@akshaysaininode.ycri0.mongodb.net/devTinder?retryWrites=true&w=majority"
+    
     MONGODB_URI
   );
 };
